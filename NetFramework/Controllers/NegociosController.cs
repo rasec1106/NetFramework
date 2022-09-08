915ffee310eda26c0dac49c898b1857f11aacad9
rasec1106 <elch.herrera@gmail.com>
2022-09-07 22:15:39 -0500

    Database connection

---
+++ NetFramework/Controllers/NegociosController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using NetFramework.Models;
+using System.Configuration;
+
+
+namespace NetFramework.Controllers
+{
+    public class NegociosController : Controller
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+        IEnumerable<Cliente> clientes(int prmIntIdPais)
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM CLIENTE WHERE idPais = @prmIntIdPais or @prmIntIdPais = 0", connection);
+            cmd.Parameters.Add("@prmIntIdPais", SqlDbType.Int);
+            cmd.Parameters["@prmIntIdPais"].Value = prmIntIdPais;
+
+            connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                clientes.Add(new Cliente()
+                {
+                    idCliente = dr.GetInt32(0),
+                    razonSocial = dr.GetString(1),
+                    direccion = dr.GetString(2),
+                    telefono = dr.GetString(3),
+                    idPais = dr.GetInt32(4),
+
+                });
+            }
+            dr.Close();
+            connection.Close();
+
+            return clientes;
+        }
+        IEnumerable<Cliente> clientes2(int prmIntIdPais, int prmIntIdCliente)
+        {
+            List<Cliente> clientes = new List<Cliente>();
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlCommand cmd = new SqlCommand("exec sp_getClientesByIdPais @prmIntIdPais, @prmIntIdPais ", connection);
+            cmd.Parameters.Add("@prmIntIdPais", SqlDbType.Int);
+            cmd.Parameters.Add("@prmIntIdCliente", SqlDbType.Int);
+            cmd.Parameters["@prmIntIdPais"].Value = prmIntIdPais;
+            cmd.Parameters["@prmIntIdCliente"].Value = prmIntIdCliente;
+
+            connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                clientes.Add(new Cliente()
+                {
+                    idCliente = dr.GetInt32(0),
+                    razonSocial = dr.GetString(1),
+                    direccion = dr.GetString(2),
+                    telefono = dr.GetString(3),
+                    idPais = dr.GetInt32(4),
+
+                });
+            }
+            dr.Close();
+            connection.Close();
+
+            return clientes;
+        }
+        // GET: Negocios
+        public ActionResult Index(int prmIntIdPais = 0, int prmIntIdCliente = 0)
+        {
+            return View(clientes2(prmIntIdPais, prmIntIdCliente));
+        }
+    }
+}
---
+++ NetFramework/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFramework.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Codigo")]
+        public int idCliente { get; set; }
+        [Display(Name = "Razon Social")]
+        public string razonSocial { get; set; }
+        [Display(Name = "Direccion")]
+        public string direccion { get; set; }
+        [Display(Name = "Telefono")]
+        public string telefono { get; set; }
+        [Display(Name = "Codigo Pais")]
+        public int idPais { get; set; }
+    }
+}
