6f634434486b1f1476b82ad73c8773b8ac82b4a6
rasec1106 <elch.herrera@gmail.com>
2022-09-07 22:52:14 -0500

    Alumno controller and view

---
+++ NetFramework/Controllers/AlumnosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using NetFramework.Models;
+using System.Configuration;
+
+namespace NetFramework.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        string cadena = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+        IEnumerable<Alumno> alumnos()
+        {
+            List<Alumno> alumnos = new List<Alumno>();
+            SqlConnection connection = new SqlConnection(cadena);
+            SqlCommand cmd = new SqlCommand("SELECT * FROM ALUMNO", connection);
+            // cmd.Parameters.Add("@prmIntIdPais", SqlDbType.Int);
+            // cmd.Parameters["@prmIntIdPais"].Value = prmIntIdPais;
+
+            connection.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                alumnos.Add(new Alumno()
+                {
+                    IdAlumno = dr.GetString(0),
+                    Apellidos = dr.GetString(1),
+                    Nombres = dr.GetString(2),
+                    Edad = dr.GetInt16(3),
+                    Sexo = dr.GetString(4),
+
+                });
+            }
+            dr.Close();
+            connection.Close();
+
+            return alumnos;
+        }
+
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            return View(alumnos());
+        }
+    }
+}
---
+++ NetFramework/Models/Alumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFramework.Models
+{
+    public class Alumno
+    {
+        [Display(Name = "Codigo")]
+        public string IdAlumno { get; set; }
+        [Display(Name = "Apellidos")]
+        public string Apellidos { get; set; }
+        [Display(Name = "Nombres")]
+        public string Nombres { get; set; }
+        [Display(Name = "Edad")]
+        public Int16 Edad { get; set; }
+        [Display(Name = "Sexo")]
+        public string Sexo { get; set; }
+    }
+}
