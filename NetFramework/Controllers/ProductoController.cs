35327f400d814a680508d66ba52a3f6a3c183db1
rasec1106 <elch.herrera@gmail.com>
2022-09-14 20:34:20 -0500

    Model, controller and view

---
+++ NetFramework/Controllers/ProductoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using NetFramework.Models;
+
+
+namespace NetFramework.Controllers
+{
+    public class ProductoController : Controller
+    {
+        IEnumerable<Producto> productos()
+        {
+            List<Producto> productos = new List<Producto>();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["connection"].ConnectionString))
+            {
+                SqlCommand cmd = new SqlCommand("exec sp_GetProductos", connection);
+                connection.Open();
+                
+                SqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    productos.Add(new Producto()
+                    {
+                        idProducto = reader.GetInt32(0),
+                        nombreProducto = reader.GetString(1),
+                        nombreCategoria = reader.GetString(2),
+                        precioUnitario = reader.GetDecimal(3),
+                        stock = reader.GetInt32(4)
+                    });
+                }
+                reader.Close();
+                connection.Close();
+            }
+            return productos;
+        }
+        // GET: Producto
+        public ActionResult Listado()
+        {
+            return View(productos());
+        }
+    }
+}
---
+++ NetFramework/Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFramework.Models
+{
+    public class Producto
+    {
+        [Display(Name = "Código")]
+        public int idProducto { get; set; }
+
+        [Display(Name = "Producto")]
+        public string nombreProducto { get; set; }
+
+        [Display(Name = "Categoría")]
+        public string nombreCategoria { get; set; }
+
+        [Display(Name = "Precio Unitario")]
+        public decimal precioUnitario { get; set; }
+
+        [Display(Name = "Stock")]
+        public int stock { get; set; }
+
+        [Display(Name = "Monto")]
+        public decimal monto { get { return precioUnitario * stock; } }
+    }
+}
