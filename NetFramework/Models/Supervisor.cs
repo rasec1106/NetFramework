93cc9d775ee3bd4cce53e4b6f91bd68ab91a7f60
rasec1106 <elch.herrera@gmail.com>
2022-09-22 00:54:51 -0500

    Create supervisor model

---
+++ NetFramework/Models/Supervisor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetFramework.Models
+{
+    public class Supervisor
+    {
+        public int idSupervisor { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Required] 
+        [Display(Name = "Apellidos")]
+        public string apellidos { get; set; }
+
+        [Display(Name = "Dirección")]
+        public string direccion { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+
+        [Required]
+        [Display(Name = "Id País")]
+        public int idPais { get; set; }
+    }
+}
